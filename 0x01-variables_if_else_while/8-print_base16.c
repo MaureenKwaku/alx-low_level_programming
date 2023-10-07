843ff5b51f64c1b3b161d30879dbbbe902ee64b2
MaureenKwaku <seyramablakwaku@gmail.com>
2023-10-07 09:39:44 -0700

    print the numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+		if (num < 10)
+		{
+			putchar('0'+ num);
+		}
+		else if (num > 9)
+		{
+			putchar('a'+ (num - 10));
+		}
+		putchar('\n');
+	}
+	return (0);
+}
