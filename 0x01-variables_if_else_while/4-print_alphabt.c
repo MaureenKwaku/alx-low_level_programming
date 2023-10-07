3029ee4aaaa8a4346e9c20b7b1d05ab11445d35c
MaureenKwaku <seyramablakwaku@gmail.com>
2023-10-06 19:43:55 -0700

    print out the alphabets while exempting q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q'&&'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
