a87371271ec9c6f995c16692c4e96e3fa5cdef25
MaureenKwaku <seyramablakwaku@gmail.com>
2023-10-07 20:29:52 -0700

    print all possible thriple combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	int i;
+	int k;
+
+	for (num = 0; num < 8; num++)
+	{
+		for (i = num + 1; i <= 9; i++)
+		{
+			for (k = i + 1; k <= 9; k++)
+			{
+				putchar('0' + num);
+				putchar('0' + i);
+				putchar('0' + k);
+
+				if (num != 8 || i != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
