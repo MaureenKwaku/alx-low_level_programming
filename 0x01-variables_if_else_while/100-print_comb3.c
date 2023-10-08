fc543f65b3d33595680647551103a49ceee34021
MaureenKwaku <seyramablakwaku@gmail.com>
2023-10-07 20:01:40 -0700

    print all possible differenet combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,21 @@
 int main(void)
 {
 	int num;
+	int i;
 
-	for (num = 0; num < 100; num++)
+	for (num = 0; num < 8; num++)
 	{
-		putchar('0' + (num / 10));
-		putchar('0' + (num % 10));
-		putchar(',');
+		for (i = num + 1; i <= 9; i++)
+		{
+			putchar('0' + num);
+			putchar('0' + i);
+			
+			if (num != 8 || i != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	return (0);
 }
