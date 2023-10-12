d8842f0b621af0392fa42307b0473abcd7ac8670
MaureenKwaku <seyramablakwaku@gmail.com>
2023-10-12 16:00:09 -0700

    multiple two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _int mul - multiples two integers
+ * @a: parameter to be checked
+ * @b: parameter to be checked
+ * Return: 1 or 0
+**/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
