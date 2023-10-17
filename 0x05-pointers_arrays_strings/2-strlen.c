7e0be17433eda5adace7d6fb41c905449ecf7f27
MaureenKwaku <seyramablakwaku@gmail.com>
2023-10-16 19:53:58 -0700

    get the lenght of the string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * swap_int - swap two integers
- *
+ *_strlen - get the lenght of a string
+ *@s: parameter to be worked on
  * Return: Always 0 (Success)
  */
 
-void swap_int(int *a, int *b)
+int _strlen(char *s)
 {
-	str
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
 }
