f6575a473bf46ea7a66d4b3173f5f041426aa1b2
MaureenKwaku <seyramablakwaku@gmail.com>
2023-10-18 21:32:50 -0700

    copy a string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcmp - copy  a string
+ * @s1: parameter to be worked on
+ * @s2: parameter to be worked on
+ * Return: dest
+**/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+			{
+				return (s1[i] - s2[i]);
+			}
+			else if (s2[i] < s1[i])
+			{
+				return (s1[i] - s2[i]);
+			}
+		}
+	}
+	return (0);
+}
