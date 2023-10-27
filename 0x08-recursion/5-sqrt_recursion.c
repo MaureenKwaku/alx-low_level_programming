2ef62636752b004685bbc442cdaeceab8a2480c9
MaureenKwaku <seyramablakwaku@gmail.com>
2023-10-26 20:38:40 -0700

    calculate the square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* sqrt- recurses to find the natura
+* @b: is an iterator
+* @a: the parameter to be checked
+* Return: 1 or 0;
+**/
+int _sqrt_a(int a, int b)
+{
+	if (b * b > a)
+	{
+		return (-1);
+	}
+	else if (b * b == a)
+	{
+		return (b);
+	}
+	return (_sqrt_a(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - return the natural square root
+ * sqrt- recurses to find the natura
+ * @n: parameter to be checked
+ * Return: 1 or 0
+**/
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_a(n, 0));
+}
