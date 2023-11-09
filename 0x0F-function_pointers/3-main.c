53b40a869d0eb486d97c16859be7eb9857dbdc1e
MaureenKwaku <seyramablakwaku@gmail.com>
2023-11-08 21:45:02 -0800

    Write a program that performs simple arithmetic operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,43 +1,50 @@
-#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
-
 /**
  * main - Entry point
  * @argc: argument count
  * @argv: argument vector
  * Return: 1 or 0
 **/
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	int (*func)(int, int);
+	char *operator;
 
 	if (argc != 4)
 	{
-		printf("Usage: %s num1 operator num2\n", argv[0]);
-		return (1);
+		printf("Error\n");
+		return (98);
 	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+	operator = argv[2];
 
-	if (argv[2][1] != '\0')
+	int (*operation[5])(int, int) = {op_add, op_sub, op_mul, op_div, op_mod};
+	char operators[5] = {'+', '-', '*', '/', '%'};
+	int index = -1;
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (*operator == operators[i])
+		{
+			index = i;
+			break;
+		}
+	}
+	if (index == -1)
 	{
 		printf("Error\n");
-		return (1);
+		return (99);
 	}
-
-	func = get_op_func(argv[2]);
-
-	if (func == NULL)
+	if (num2 == 0 && (index == 3 || index == 4))
 	{
 		printf("Error\n");
-		return (1);
+		return (100);
 	}
-
-	result = func(num1, num2);
+	result = operation[index](num1, num2);
 	printf("%d\n", result);
-
 	return (0);
 }
