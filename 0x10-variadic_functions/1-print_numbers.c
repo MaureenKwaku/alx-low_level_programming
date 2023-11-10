d1e1443ed8a9d6f972118f53ebf694704e70c24d
MaureenKwaku <seyramablakwaku@gmail.com>
2023-11-09 18:45:26 -0800

    Write a function that prints numbers, followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print integers
+ * @separator: parameter to be checked
+ * @n: parameter to be checked
+ * Return: 1 or 0
+**/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, arr;
+
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		arr = va_arg(ptr, const unsigned int);
+		printf("%d", arr);
+
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ptr);
+}
