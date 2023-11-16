a7efa4d2e3ca04fb68f30f9068dfd1bf1e89b800
MaureenKwaku <seyramablakwaku@gmail.com>
2023-11-16 15:32:37 -0800

    Write a 64-bit program to print Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-section .data
-    hello_msg db "Hello, Holberton", 0
-
-section .text
-    global _start
-
-_start:
-    ; Set up stack frame for calling printf
-    push rbp
-    mov rdi, hello_msg  ; Address of the string
-    call printf         ; Call printf function
-    add rsp, 8          ; Adjust stack pointer after calling a function
-
-    ; Exit the program
-    mov eax, 60         ; syscall number for exit
-    xor edi, edi        ; exit code 0
-    syscall
-
+global    main 
+           extern    printf 
+ main: 
+           mov   edi, format 
+           xor   eax, eax 
+           call  printf 
+           mov         eax, 0 
+           ret 
+ format: db `Hello, Holberton\n`,0
