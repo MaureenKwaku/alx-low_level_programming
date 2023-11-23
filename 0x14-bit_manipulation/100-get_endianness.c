47f3881af490f7324918cd2ffbb01b6b8fcb121f
MaureenKwaku <seyramablakwaku@gmail.com>
2023-11-23 13:50:17 -0800

    Check for endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -12,24 +12,3 @@ int get_endianness(void)
 
 	return ((int)(*c));
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	int n = get_endianness();
-
-	if (n != 0)
-	{
-		printf("Little Endian\n");
-	}
-	else
-	{
-		printf("Big Endian\n");
-	}
-	return (0);
-}
